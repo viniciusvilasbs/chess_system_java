71dfc021d0327959da3e7445cb187ef490b67b1a
Vinicius VBS <vinyvilas@gmail.com>
2022-05-23 22:39:37 -0300

    First class: Position

--- src/application/ProgramJogoXadrez.java
+++ src/application/ProgramJogoXadrez.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import boardGame.Position;
+
 public class ProgramJogoXadrez {
 
 	public static void main(String[] args) {
@@ -10,7 +12,8 @@ public class ProgramJogoXadrez {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 		
 		
 		sc.close();

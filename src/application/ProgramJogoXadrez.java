738e8d2515a92d1522ed1ac2ec0d670811519fb2
Vinicius VBS <vinyvilas@gmail.com>
2022-05-24 09:08:50 -0300

    Chess layer and printing the board

--- src/application/ProgramJogoXadrez.java
+++ src/application/ProgramJogoXadrez.java
@@ -1,12 +1,14 @@
 package application;
 
-import boardGame.Board;
+import chess.ChessMatch;
 
 public class ProgramJogoXadrez {
 
 	public static void main(String[] args) {
 		
-		Board board = new Board(8, 8);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+		
 		
 	}
 
--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -20,13 +20,20 @@ public class Board {
 		this.columns = columns;
 	}
 
-	public Piece[][] getPieces() {
-		return pieces;
+	public int getRows() {
+		return rows;
 	}
 
-	public void setPieces(Piece[][] pieces) {
-		this.pieces = pieces;
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
 	}
 	
-	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
+	 
 }
